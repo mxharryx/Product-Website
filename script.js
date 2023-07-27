37b4f5213d0b91841a1446be4cf22417c9554471
Mohammed Harris <57700846+mxharryx@users.noreply.github.com>
2023-07-27 16:38:42 -0400

    Product Section changes

--- script.js
+++ script.js
@@ -3,33 +3,71 @@
 const productsContainer = document.querySelector('.products .container');
 
 const productsData = [
-    {name: 'Shoe 1', price: '$120.00', image: 'shoe1.jpg'},
-    {name: 'Shoe 2', price: '$150.00', image: 'shoe1.jpg'},
-    {name: 'Shoe 3', price: '$280.00', image: 'shoe1.jpg'},
+    {
+        title: "Zippety Zap",
+        price: "$419.99",
+        image: "assets/shoe1.png"
+    },
+    {
+        title: "Zuppety Zip",
+        price: "$599.99",
+        image: "assets/shoe2.png"
+    },
+    {
+        title: "Zappety Zup",
+        price: "$899.99",
+        image: "assets/shoe3.png"
+    },
+    // Add more products here as needed
 ];
 
-//Fn to create a product card
+const productCardsContainer = document.getElementById("productCards");
 
 function createProductCard(product) {
-    const card=document.createElement('div');
-    card.classList.add(product-card);
+    const card = document.createElement("div");
+    card.classList.add("product-card");
 
-    const image = document.createElement('img');
+    const image = document.createElement("img");
+    image.classList.add("product-image");
     image.src = product.image;
+    image.alt = product.title;
     card.appendChild(image);
 
-    const productName = document.createElement('h3');
-    productName.textContent = product.name;
-    card.appendChild(productName);
+    const title = document.createElement("h3");
+    title.classList.add("product-title");
+    title.textContent = product.title;
+    card.appendChild(title);
 
-    const productPrice = document.createElement('p');
-    productPrice.textContent = product.price;
-    card.appendChild(productPrice);
+    const price = document.createElement("p");
+    price.classList.add("product-price");
+    price.textContent = product.price;
+    card.appendChild(price);
 
     return card;
 }
 
-productsData.forEach((product) => {
-    const card = createProductCard(product);
-    productsContainer.appendChild(card);
+// Generate product cards and append to the container
+productsData.forEach(product => {
+    const productCard = createProductCard(product);
+    productCardsContainer.appendChild(productCard);
 });
+
+const rotatingImages =[
+    "assets/shoe1.png",
+    "assets/shoe2.png",
+    "assets/shoe3.png",
+    "assets/shoe4.png",
+];
+
+let currentImageIndex = 0;
+const rotatingImage = document.getElementById("rotatingImage");
+
+function changeImage() {
+    currentImageIndex = (currentImageIndex + 1) % rotatingImages.length;
+    rotatingImage.src = rotatingImages[currentImageIndex];
+}
+// Change image every 5 seconds
+setInterval(changeImage, 5000);
+
+
+
